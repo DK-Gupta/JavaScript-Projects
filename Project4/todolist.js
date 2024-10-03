1e7567de0ab038917737a53aa43cf1e7adad75a4
Dhirendra kumar gupta <dkg700731@gmail.com>
2024-10-03 11:50:05 +0530

    More progress in todo list

--- Project4/todolist.js
+++ Project4/todolist.js
@@ -2,13 +2,23 @@ const addTaskBtn = document.getElementById('addTaskBtn');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 
-
+let tasks =[];
 addTaskBtn.addEventListener('click', function() {
     const taskText = taskInput.value;
 
     if (taskText !== "") {
+        tasks.push(taskText);
+        updatelist();
+        
+    }
+    else{
+        alert("Please enter a task")
+    }
+
+    function updatelist(){
         const newTask = document.createElement('li');
         newTask.textContent = taskText;
+        
 
         // Add remove button for each task
         const removeBtn = document.createElement('button');
@@ -22,8 +32,5 @@ addTaskBtn.addEventListener('click', function() {
 
         // Clear input field
         taskInput.value = "";
-    }
-    else{
-        alert("Please enter a task")
     }
 });
