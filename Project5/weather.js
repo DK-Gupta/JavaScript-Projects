85a7a0ea2d13feadb99caca72b66c0f63d10bd22
Dhirendra kumar gupta <dkg700731@gmail.com>
2024-10-16 12:21:03 +0530

    weather api call completed

--- Project5/weather.js
+++ Project5/weather.js
@@ -4,17 +4,17 @@ const InputText = document.getElementById('InputText')
 const CityName = document.getElementById('CityName')
 const Temp = document.getElementById('Temp')
 
-async function data(CityName){
-    const result = await fetch( `https://api.weatherapi.com/v1/current.json?key=9b5c80aaf09e498b95b172935241510&q=${value}&aqi=yes`)
-    console.log(result);
+async function data(Cityname){
+    const result = await fetch( `https://api.weatherapi.com/v1/current.json?key=9b5c80aaf09e498b95b172935241510&q=${Cityname}&aqi=yes`)
+    
     return  await result.json();
-}
+};
 
 Search.addEventListener("click",async ()=>{
     const value = InputText.value;
     const output = await data(value);
-    CityName.innerText = `${output.location.name},${output.location.origin}`;
-    Temp.innerText = Temp.current.temp_c;
+    CityName.innerText = `${output.location.name},${output.location.country}`;
+    Temp.innerText = output.current.temp_f;
 
 
-})
+});
